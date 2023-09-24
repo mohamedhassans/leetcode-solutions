ab375d5eab888f7d9f1e94ceaf2d3213cff343c8
Mohamed Hassan <mohamedhassan2082002@hotmail.com>
2023-09-24 10:49:19 +0300

    Time: 2 ms (82.59%), Space: 40.2 MB (92.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(st.isEmpty()){
+                st.push(s.charAt(i));
+            }
+            else if((s.charAt(i)==')'&&st.peek()=='(')|| 
+            (s.charAt(i)==']'&&st.peek()=='[')||
+            (s.charAt(i)=='}'&&st.peek()=='{') 
+            ){
+st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+
+        }
+        return st.isEmpty();
+    }
+}
