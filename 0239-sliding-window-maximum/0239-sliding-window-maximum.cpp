3c49ae38bdc77af40e4ae5f60d6561ea77109bf8
Mohamed Hassan <mohamedhassan2082002@hotmail.com>
2024-02-10 03:11:17 +0200

    Time: 175 ms (86.43%), Space: 132.4 MB (97.13%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int> ans(n-k+1,0);
-        set<pair<int,int>> s;
-        for(int i=0;i<k;i++)
-             s.insert({nums[i],i});
-        ans[0]=s.rbegin()->first;
-        for(int i=k;i<n;i++){
-            s.erase({nums[i-k],i-k});
-            s.insert({nums[i],i});
-            ans[i-k+1]=s.rbegin()->first;
+    int n=nums.size();
+    vector<int> ans(n-k+1,0);
+    deque<int> curmax;
+    for(int i=0;i<k;i++){
+        while(!curmax.empty()&&nums[i]>curmax.back()){
+            curmax.pop_back();
         }
-        return ans;
-        
+        curmax.push_back(nums[i]);
     }
+    ans[0]=curmax.front();
+    for(int i=k ;i<n;i++){
+        if(curmax.front()==nums[i-k]){
+            curmax.pop_front();
+        }
+        while(!curmax.empty()&&nums[i]>curmax.back()){
+            curmax.pop_back();
+        }
+        curmax.push_back(nums[i]);
+        ans[i-k+1]=curmax.front();
+    }
+    return ans;
+
+}
 };
